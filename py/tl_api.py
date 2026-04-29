74becd3337348d16d929fa841e184bb86b28db23
DPayne25 <32944319+DPayne25@users.noreply.github.com>
2026-04-29 04:35:08 -0400

    download order history

--- py/tl_api.py
+++ py/tl_api.py
@@ -1,4 +1,4 @@
-import requests, configparser
+import requests, configparser, json
 from pathlib import Path
 
 config = configparser.ConfigParser()
@@ -25,6 +25,7 @@ access_token = requests.post(url_access_token, json=payload, headers=headers)
 print(access_token.text)
 print(access_token.json()["accessToken"])
 print(access_token.status_code)
+bearer_authorization = "Bearer {}".format(access_token.json()["accessToken"])
 
 # ===========================================================================================================================================
 # ===========================================================================================================================================
@@ -36,9 +37,33 @@ print(access_token.status_code)
 
 headers_all_accounts = {
     "accept": "application/json",
-    "authorization": "Bearer {}".format(access_token.json()["accessToken"])
+    "authorization": bearer_authorization
 }
 
 
 all_accounts = requests.get("{}/backend-api/auth/jwt/all-accounts".format(config['tradelocker']['TL_URL']), headers=headers_all_accounts)
 print(all_accounts.text)
+
+# ===========================================================================================================================================
+# ===========================================================================================================================================
+
+
+# ===========================================================================================================================================
+# Order history
+# ===========================================================================================================================================
+
+base_url = config['tradelocker']['TL_URL']
+accountID = config['tradelocker']['TL_ACCOUNT_ID']
+headers_order_history = {
+    "accNum": "1",
+    "accept": "application/json",
+    "authorization": bearer_authorization
+}
+
+
+order_history_get = requests.get(f"{base_url}/backend-api/trade/accounts/{accountID}/ordersHistory", headers=headers_order_history)
+
+order_history = order_history_get.json()
+
+with open('output.json', 'w') as f:
+    json.dump(order_history, f, indent=2)
